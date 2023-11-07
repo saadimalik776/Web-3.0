464046df8a2b897e616deef1a1d1ea119c64b076
saadimalik776 <118857331+saadimalik776@users.noreply.github.com>
2023-11-07 19:36:18 +0500

    Update q12.ts

--- Typescript/q12.ts
+++ Typescript/q12.ts
@@ -1,8 +1,12 @@
 // Problem 12: Greetings
 console.log(`******* Problem 12 *******`);
-let friend_names: string[] = ["Saad", "Esha", "Sadia", "Majid", "Ali"];
-console.log(`Good Morning ${friend_names[0]}!`);
-console.log(`Good Morning ${friend_names[1]}!`);
-console.log(`Good Morning ${friend_names[2]}!`);
-console.log(`Good Morning ${friend_names[3]}!`);
-console.log(`Good Morning ${friend_names[4]}!`);
+const names = ['Ali', 'Esha', 'Saad', 'Munaf'];
+console.log("Names:");
+for (const name of names) {
+    console.log(name);
+}
+console.log("\nGreetings:");
+const commonMessage = "Hello, nice to meet you, ";
+for (const name of names) {
+    console.log(commonMessage + name + "!");
+}
