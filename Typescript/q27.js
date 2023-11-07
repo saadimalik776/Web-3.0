58b3df44ef2fd6d1a32f3563b7f763a798ad5692
saadimalik776 <118857331+saadimalik776@users.noreply.github.com>
2023-11-07 19:41:35 +0500

    Update q27.js

--- Typescript/q27.js
+++ Typescript/q27.js
@@ -1,14 +1,10 @@
 // Problem 27: Alien Colors#3
-console.log("******* Problem 25,26 & 27 *******");
-var alien_color;
-if (alien_color === 'Green') {
-    console.log("The player has earned 5 points");
-}
-else if (alien_color === 'Orange') {
-    console.log("The player has earned 10 points");
-}
-else
-    (alien_color === 'red');
-{
-    console.log("The player has earned 15 points");
+console.log(`******* Problem 25,26 & 27 *******`); 
+var alien_color = 'red';
+if (alien_color === 'green') {
+    console.log('You just earned 5 points!');
+} else if (alien_color === 'yellow') {
+    console.log('You just earned 10 points!');
+} else if (alien_color === 'red') {
+    console.log('You just earned 15 points!');
 }
