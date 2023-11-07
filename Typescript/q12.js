26124282fbb2986596aafcd670c25eaef050a2d1
saadimalik776 <118857331+saadimalik776@users.noreply.github.com>
2023-11-07 19:37:57 +0500

    Update q12.js

--- Typescript/q12.js
+++ Typescript/q12.js
@@ -1,8 +1,13 @@
 // Problem 12: Greetings
 console.log("******* Problem 12 *******");
-var friend_names = ["Saad", "Esha", "Sadia", "Majid", "Ali"];
-console.log("Good Morning ".concat(friend_names[0], "!"));
-console.log("Good Morning ".concat(friend_names[1], "!"));
-console.log("Good Morning ".concat(friend_names[2], "!"));
-console.log("Good Morning ".concat(friend_names[3], "!"));
-console.log("Good Morning ".concat(friend_names[4], "!"));
+const names = ['Ali', 'Esha', 'Saad', 'Munaf'];
+console.log("Names:");
+for (const name of names) {
+    console.log(name);
+}
+console.log("\nGreetings:");
+const commonMessage = "Hello, nice to meet you, ";
+for (const name of names) {
+    console.log(commonMessage + name + "!");
+}
+
