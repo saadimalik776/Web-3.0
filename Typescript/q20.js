13ef0c3938b53782a6a639734a6b45391a8b57f3
saadimalik776 <118857331+saadimalik776@users.noreply.github.com>
2023-11-15 09:20:15 +0500

    Update q20.js

--- Typescript/q20.js
+++ Typescript/q20.js
@@ -1,18 +1,16 @@
-// Problem 20: 
 console.log(`******* Problem 20 *******`);
-const countries: string[] = [
-    "Madina",
-    "Arabic",
-    "Paksitan",
-    "Saudia",
-    "Turkey",
-    "France",
-    "Germany",
-    "Japan",
-    "China",
-    "Brazil",
-    "India"
-];
-for (const country of countries) {
-    console.log(country);
-}
+// Create an array of countries with their cities
+const countriesWithCities: [string, string[]][] = [
+    ["Saudia", ["Madina", "Makkah", "Riyadh"]],
+    ["Pakistan", ["Kashmir", "Sargodha", "Bhera"]],
+    ["India", ["Dehli", "Ajmar Shreeef", "Breali"]],
+  ];
+  console.log("List of Countries with Cities:");
+  console.log("countries:  Cities \n ")
+  for (const countryData of countriesWithCities) {
+    const country: string = countryData[0];
+    const cities: string[] = countryData[1];
+  
+    console.log(`${country}: ${cities.join(", ")}`);
+  }
+  
