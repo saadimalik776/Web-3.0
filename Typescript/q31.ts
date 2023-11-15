fa8aa803a8021ace8ae76cb9206377168915dc2d
saadimalik776 <118857331+saadimalik776@users.noreply.github.com>
2023-11-15 09:23:44 +0500

    Update q31.ts

--- Typescript/q31.ts
+++ Typescript/q31.ts
@@ -1,27 +1,27 @@
-// Problem 31: No users
+ // Problem 31: No users
 console.log(`******* Problem 31 *******`);
-let usernames: string[] = ["admin", "Eric", "John", "Alice", "Bob"];
+// Create a list of users
+let users = ["Esha", "Saad", "Munaf"];
 
-if (usernames.length === 0)
-{
-    console.log("We need to find some users!");
-} else
- {
-    for (let username of usernames) 
- {
-        if (username === "admin") 
-{
-      console.log("Hello admin, would you like to see a status report?");
+if (users.length > 0) {
+    console.log("List of Users:");
+    for (let user of users) {
+        console.log(user );
+     }
+     console.log("\n")  
 } else 
 {
-      console.log(`Hello ${username}, thank you for logging in again.`);
- }
- }
+    console.log("We need to find some users!");
 }
 
-// Remove all usernames from the array
-usernames = [];
-
-if (usernames.length === 0) {
-    console.log("We need to find some users!");
+users = [];
+if (users.length > 0) {
+    // Print the list of users
+    console.log("\nList of Users after removal:");
+    for (let user of users) {
+        console.log(user);
+    }
+} else {
+console.log("after user remove  ")
+    console.log("\nWe need to find some users!");
 }
